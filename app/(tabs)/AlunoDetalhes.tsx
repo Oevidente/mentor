9f6b233447cd149bb99d15a81d25fd530163c8ed
André Luiz Alves <andreluiz1902@gmail.com>
2024-10-25 16:17:06 -0300

    Página individual do aluno funcional

--- app/(tabs)/AlunoDetalhes.tsx
+++ app/(tabs)/AlunoDetalhes.tsx
@@ -1,61 +1,18 @@
-/*import React, { useContext } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
-import { useRouter, useSearchParams } from 'expo-router';
-import { AlunosContext } from './AlunoContext';
-
-const AlunoDetalhes = () => {
-  const { id } = useSearchParams();
-  const { alunos } = useContext(AlunosContext);
-  const aluno = alunos.find((aluno) => aluno.id === id);
-  const router = useRouter();
-
-  if (!aluno) {
-    return (
-      <View style={styles.container}>
-        <Text>Aluno não encontrado</Text>
-      </View>
-    );
-  }
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>{aluno.nome}</Text>
-      <Text>Email: {aluno.email}</Text>
-      <Text>
-        Telefone: ({aluno.telefone.slice(0, 2)}) {aluno.telefone.slice(2, 7)}-
-        {aluno.telefone.slice(7)}
-      </Text>
-      <Button title="Voltar" onPress={() => router.back()} />
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: '#fff',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 16,
-  },
-});
-
-export default AlunoDetalhes;
-*/
-
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
-import { useRouter, useSearchParams } from 'expo-router';
+import { View, Text, StyleSheet } from 'react-native';
+import { useRoute } from '@react-navigation/native';
 import { AlunosContext } from './AlunoContext';
 
 const AlunoDetalhes = () => {
-  const { id } = useSearchParams();
+  const route = useRoute();
+  const { id } = route.params || {}; // Verifica se params está definido antes de acessar id
   const { alunos } = useContext(AlunosContext);
+
+  console.log('Route Params:', route.params);
+  console.log('ID:', id);
+  console.log('Alunos:', alunos);
+
   const aluno = alunos.find((aluno) => aluno.id === id);
-  const router = useRouter();
 
   if (!aluno) {
     return (
@@ -73,7 +30,6 @@ const AlunoDetalhes = () => {
         Telefone: ({aluno.telefone.slice(0, 2)}) {aluno.telefone.slice(2, 7)}-
         {aluno.telefone.slice(7)}
       </Text>
-      <Button title="Voltar" onPress={() => router.back()} />
     </View>
   );
 };
